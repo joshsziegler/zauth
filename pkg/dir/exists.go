d5ef9825894583ff2a189fe7f9bb7577a225a3e3
Josh Ziegler <josh.s.ziegler@gmail.com>
2019-11-25 15:29:48 -0500

    Move DirExists to pkg/dir/ and rename to Exists()

--- pkg/dir/exists.go
+++ pkg/dir/exists.go
@@ -2,8 +2,8 @@ package main
 
 import "os"
 
-// DirExists reports whether the path is a directory and exists as a boolean
-func DirExists(path string) bool {
+// Exists reports whether the path is a directory and exists as a boolean
+func Exists(path string) bool {
 	file, err := os.Stat(path)
 	if err == nil {
 		if file.Mode().IsDir() {
