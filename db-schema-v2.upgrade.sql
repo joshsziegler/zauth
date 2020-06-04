ccfc2bf1ba598b754cc522709f6cbff4ab08bc37
Josh Ziegler <josh.s.ziegler@gmail.com>
2020-06-04 16:29:33 -0400

    Add ON DELETE rules to our MySQL foreign keys

--- db-schema-v2.upgrade.sql
+++ db-schema-v2.upgrade.sql
@@ -12,6 +12,14 @@
 -- Groups is now a MySQL reserved keyword, so we must rename our table
 RENAME Table `Groups` TO UserGroups;
 
+-- Change UserGroups.UserID FK to delete rows when the User is deleted
+ALTER TABLE `User2Group` DROP FOREIGN KEY `User2Group_ibfk_1`;
+ALTER TABLE `User2Group` ADD CONSTRAINT `User2Group_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`) ON DELETE CASCADE;
+
+-- Change UserGroups.GroupID FK to delete rows when the Group is deleted
+ALTER TABLE `User2Group` DROP FOREIGN KEY `User2Group_ibfk_2`;
+ALTER TABLE `User2Group` ADD CONSTRAINT `User2Group_ibfk_2` FOREIGN KEY (`GroupID`) REFERENCES `UserGroups` (`ID`) ON DELETE CASCADE;
+
 SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `Folders`;
@@ -20,15 +28,15 @@ CREATE TABLE `Folders` (
   `FolderID` int(11) DEFAULT NULL, -- NULL folder means it's in the base of the project/Group
   `Name` varchar(255) NOT NULL,
   `CreatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `CreatedBy` int(11) NOT NULL, -- UserID
+  `CreatedBy` int(11), -- UserID
   `UpdatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `UpdatedBy` int(11) NOT NULL,
+  `UpdatedBy` int(11),
   PRIMARY KEY (`ID`),
   UNIQUE KEY `UniqName` (`Name`, `FolderID`),
   FULLTEXT(`Name`),
-   CONSTRAINT `FolderFk` FOREIGN KEY (`FolderID`) REFERENCES `Folders` (`ID`),
-  CONSTRAINT `CreatedByFk1` FOREIGN KEY (`CreatedBy`) REFERENCES `Users` (`ID`),
-  CONSTRAINT `UpdatedByFk1` FOREIGN KEY (`UpdatedBy`) REFERENCES `Users` (`ID`)
+   CONSTRAINT `FolderFk` FOREIGN KEY (`FolderID`) REFERENCES `Folders` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `CreatedByFk1` FOREIGN KEY (`CreatedBy`) REFERENCES `Users` (`ID`) ON DELETE SET NULL,
+  CONSTRAINT `UpdatedByFk1` FOREIGN KEY (`UpdatedBy`) REFERENCES `Users` (`ID`) ON DELETE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `Files`;
@@ -42,16 +50,16 @@ CREATE TABLE `Files` (
   `Digest` varchar(64) NOT NULL,
   -- MimeType ??
   `CreatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `CreatedBy` int(11) NOT NULL, -- UserID
+  `CreatedBy` int(11), -- UserID
   `UpdatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `UpdatedBy` int(11) NOT NULL,
+  `UpdatedBy` int(11),
   PRIMARY KEY (`ID`),
   UNIQUE KEY `UniqName` (`Name`, `FolderID`),
   FULLTEXT(`Name`),
-  CONSTRAINT `GroupFk` FOREIGN KEY (`GroupID`) REFERENCES `UserGroups` (`ID`),
-  CONSTRAINT `FolderFk2` FOREIGN KEY (`FolderID`) REFERENCES `Folders` (`ID`),
-  CONSTRAINT `CreatedByFk2` FOREIGN KEY (`CreatedBy`) REFERENCES `Users` (`ID`),
-  CONSTRAINT `UpdatedByFk2` FOREIGN KEY (`UpdatedBy`) REFERENCES `Users` (`ID`)
+  CONSTRAINT `GroupFk` FOREIGN KEY (`GroupID`) REFERENCES `UserGroups` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `FolderFk2` FOREIGN KEY (`FolderID`) REFERENCES `Folders` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `CreatedByFk2` FOREIGN KEY (`CreatedBy`) REFERENCES `Users` (`ID`) ON DELETE SET NULL,
+  CONSTRAINT `UpdatedByFk2` FOREIGN KEY (`UpdatedBy`) REFERENCES `Users` (`ID`) ON DELETE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 SET FOREIGN_KEY_CHECKS = 1;
