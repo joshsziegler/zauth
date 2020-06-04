a0dfd367444313309a04b8db8e78d2ac9801641a
Josh Ziegler <josh.s.ziegler@gmail.com>
2020-06-04 17:34:17 -0400

    Use middleware to return and write errors in file upload handler

--- models/httpserver/fileUpload.go
+++ models/httpserver/fileUpload.go
@@ -6,17 +6,24 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"path"
+
+	"github.com/ansel1/merry"
 )
 
 const (
+	UploadFolder = "uploads"
 	KiB          = 1024
 	MiB          = KiB * 1024
 	GiB          = MiB * 1024
-	UploadFolder = "uploads"
+	TiB          = GiB * 1024
+)
+
+var (
+	ErrUploadBadRequest = merry.WithMessage(ErrBadRequest, "file upload failed due to bad request")
+	ErrUploadBadWrite   = merry.WithMessage(ErrInternal, "file upload failed: could not save file to disk")
 )
 
 // FileUploadPost handles uploading one or more files from a user, to a Group.
@@ -24,41 +31,32 @@ func FileUploadPost(c *Context, w http.ResponseWriter, r *http.Request) error {
 	// TODO: Check permissions and arguments
 
 	// Check for uploads directory, and create it if necessary
+	// TODO: Is there a potential race-condition here, with the dir being
+	//       deleted before the file(s) are created? - JZ
 	if _, err := os.Stat(UploadFolder); os.IsNotExist(err) {
 		os.Mkdir(UploadFolder, 0700)
 	}
 
+	// Parse multipart file upload
 	var Buf bytes.Buffer
-
-	// Parse multipart file upload,  32 << 20 specifies a maximum size of 32 MB
-	r.ParseMultipartForm(GiB * 2)
+	r.ParseMultipartForm(GiB * 10) // TODO: Allow this be configurable
 	files := r.MultipartForm.File["file"]
 	for _, fileHeader := range files {
 		file, err := fileHeader.Open()
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest) // ~ Code 400
-			io.WriteString(w, "Error: Failed to get get file from upload request\n")
-			log.Printf("Failed to get file upload from client request:\n %s", err)
-			return err
+			return merry.Here(ErrUploadBadRequest)
 		}
 		defer file.Close()
-
 		// Copy the file data to the buffer
 		io.Copy(&Buf, file)
 		fileBytes := Buf.Bytes()
 		// Convert the byte array Hash to a String
 		hashString := fmt.Sprintf("%x", sha256.Sum256(fileBytes))
-		log.Printf("File: %v    SHA 256: %s\n", fileHeader.Filename, hashString)
-
 		// Write the upload to disk
 		err = ioutil.WriteFile(path.Join(UploadFolder, hashString), fileBytes, 0640)
 		if err != nil {
-			log.Printf("Failed to write file to disk:\n%s", err)
-			w.WriteHeader(http.StatusInternalServerError) // ~ Code 500
-			io.WriteString(w, "Error: Failed to save uploaded file to disk\n")
-			return err
+			return merry.Here(ErrUploadBadWrite)
 		}
-
 		// Reset buffer so we can use it again (reduces memory allocations)
 		Buf.Reset()
 	}
